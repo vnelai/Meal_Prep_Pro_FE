7bb07f424a1df4d64e813b45dd6c9768216ffd93
vnelai <118484488+vnelai@users.noreply.github.com>
2025-01-30 15:01:53 -0500

    Add the Hero component on the Home page so we can stage the home page that will include the hero component in the app.jsx

---
+++ src/pages/home/Home.jsx
@@ -0,0 +1,17 @@
+// Import Modules
+import React from "react";  // Import React
+import Hero from './components/hero/Hero';  // Import Hero component
+
+
+// Home page function
+function Home() {
+  return (
+    <div>
+        <Hero/>
+    </div>
+  );
+};
+
+
+// Export Home page
+export default Home;
