fc89c05ce5d807bf84db358ec59fac5916cb1830
vnelai <118484488+vnelai@users.noreply.github.com>
2025-01-30 15:56:36 -0500

    Import RecipeList component into Recipes page and stage component

--- src/pages/recipes/Recipes.jsx
+++ src/pages/recipes/Recipes.jsx
@@ -2,15 +2,17 @@
 import React from 'react'  // Import react
 // Import RecipeSearch component
 import RecipeSearch from '../../components/recipe_search/RecipeSearch'
+import RecipeList from './RecipeList'; // Import RecipeList component
 
 function Recipes() {
   return (
     <div>
         <h2>Explore Recipes</h2>
-        <RecipeSearch/>
+        <RecipeSearch/>  
+        <RecipeList/>
     </div>
   )
 }
 
 // Export Recipes
-export default Recipes
+export default Recipes;
