10a9a63ac35a24b50b6b8eb6e6b3e8a9dcaa02d9
vnelai <118484488+vnelai@users.noreply.github.com>
2025-01-30 10:44:31 -0500

    Build structure of navigation bar

---
+++ src/components/nav/Nav.jsx
@@ -0,0 +1,38 @@
+// Import modules
+import React from "react";  // Import react module
+ // Import Link component, helps with navigation 
+ // //to other pages without triggering full page reload
+import { Link } from 'react-router-dom';
+import './Nav.css';  // Import Nav css file for styling
+
+
+function Nav() {
+  return (
+    <nav className="menu">
+        <ul>
+            <li>
+                {/* Home Page where hero and featured will be */}
+                <Link to='/' className="menu-link">Home</Link>
+            </li>
+            <li>
+                {/* Recipes page where users can explore recipes from external API */}
+                <Link to='/recipes' className="menu-link">Recipes</Link>
+            </li>
+            <li>
+                {/* Favorites page that will display all users favorites even custom recipes */}
+                <Link to='/favorites' className="menu-link">Favorites</Link>
+            </li>
+            <li>
+                {/* Meal planner page where user can set a schedule for their meals */}
+                <Link to='/meal-planner' className="menu-link">Meal Planner</Link>
+            </li>
+            <li>
+                {/* Shopping list page allows users to save needed ingredients for their favorite recipes */}
+                <Link to='/shopping-list' className="menu-link">Shopping List</Link>
+            </li>
+        </ul>
+    </nav>
+  );
+};
+
+export default Nav
