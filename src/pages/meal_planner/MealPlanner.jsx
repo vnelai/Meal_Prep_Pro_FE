3821d612d28521d1c6a30569edb7aff6dcd732c7
vnelai <118484488+vnelai@users.noreply.github.com>
2025-01-31 15:38:23 -0500

    Add the favorites prop and state to MealPlannerPage in order to be passed down to child component MealPlanner

--- src/pages/meal_planner/MealPlanner.jsx
+++ src/pages/meal_planner/MealPlanner.jsx
@@ -1,15 +1,32 @@
 // Import Modules
-import React from 'react';  // Import React
+//Import react, useState, and useEffect
+import React, { useState, useEffect } from "react";
 // Import MealPlanner component
 import MealPlanner from '../../components/meal_planner/MealPlanner';
 
 
 // MealPlanner page function
 function MealPlannerPage() {
+    const [favorites, setFavorites]= useState([]);
+
+    useEffect(() => {
+        //Fetch favorites meals from backend
+        async function fetchFavorites() {
+            try {
+                const res = await fetch('/api/favorites');
+                const data = await res.json();
+                setFavorites(data);  // Set results in json format
+            } catch (error) {
+                console.error ("Failed to fetch favorite recipes:", error);
+            }
+        }
+        fetchFavorites(); //Call fetch function
+    }, []);
+
   return (
     <div>
         <h2>Meal Planner</h2>
-        <MealPlanner/>
+        <MealPlanner favorites={favorites}/>
     </div>
   );
 }
