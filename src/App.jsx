95160bd8fe2a8a579fff03876664f107e48c8706
vnelai <118484488+vnelai@users.noreply.github.com>
2025-01-31 13:07:01 -0500

    Import and stage the Favorites page into App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import './App.css' // Import app css file for styling
 import Nav from './components/nav/Nav';  // Import Nav component
 import Home from './pages/home/Home';  // Import Home page
 import Recipes from './pages/recipes/Recipes';  // Import Recipes page
-
+import Favorites from './pages/favorites/Favorites';  // Import Favorites page
 
 // App.jsx function 
 function App() {
@@ -18,6 +18,8 @@ function App() {
         <Route path='/' element={<Home/>}/> 
         {/* Add Recipes page route */}
         <Route path='/recipes' element={<Recipes/>}/>
+        {/* Add Favorites page route */}
+        <Route path='/favorites' element={<Favorites/>}/>
       </Routes>
     </>
   );
