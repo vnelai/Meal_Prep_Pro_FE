dd1095a42ff1714165d17b31518b76ec9c99731e
vnelai <118484488+vnelai@users.noreply.github.com>
2025-01-31 15:17:46 -0500

    Build and export the MealPlanner Page. Also import and stage the MealPlanner component to the page

---
+++ src/pages/meal_planner/MealPlanner.jsx
@@ -0,0 +1,18 @@
+// Import Modules
+import React from 'react';  // Import React
+// Import MealPlanner component
+import MealPlanner from '../../components/meal_planner/MealPlanner';
+
+
+// MealPlanner page function
+function MealPlanner() {
+  return (
+    <div>
+        <h2>Meal Planner</h2>
+        <MealPlanner/>
+    </div>
+  );
+}
+
+// Export MealPlanner page
+export default MealPlanner
