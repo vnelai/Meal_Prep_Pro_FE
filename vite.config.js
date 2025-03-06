75918d743374e610a5fba2903894026b4eec1400
vnelai <118484488+vnelai@users.noreply.github.com>
2025-03-06 12:58:30 -0500

    fix: allow 'meal-prep-pro-fe.onrender.com' in preview.allowedHosts

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,10 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    host: '0.0.0.0',  // Make sure it's listening on all network interfaces
-    port: 3000         // You can change the port if needed
+    host: '0.0.0.0',
+    port: 3000
+  },
+  preview: {
+    allowedHosts: ['meal-prep-pro-fe.onrender.com']
   }
 })
